66cbc2b4cc391336ce4bd6586b1ceea73253e529
willjbraga <williamjubraga@gmail.com>
2025-03-16 17:28:12 -0300

    Inserindo dados no BD
    Inserindo dados usando o comando DML Insert

--- Comandos.sql
+++ Comandos.sql
@@ -6,7 +6,7 @@ USE universidade_u;
 
 /*Criação da Tabela Aluno usando a modelagem fisica gerada pelo BR Modelo*/
 
-CREATE TABLE Aluno (
+CREATE TABLE ALUNO (
     endereco text,
     sexo char(1),
     idade int,
@@ -17,4 +17,30 @@ CREATE TABLE Aluno (
     ativo_sn int
 );
 
+insert into aluno(
+	sexo, idade, data_inscricao_curso, telefone, valor_pago_curso, ativo_sn, endereco, nome)
+values(
+	'M', 55, '2018-12-01', '11 5555-2222', 645.22, 1, 
+    'Rua Crodovaldo, 1500, ap525 - SP', 'Zé'
+);
 
+insert into aluno(
+	sexo, idade, data_inscricao_curso, telefone, valor_pago_curso, ativo_sn, endereco, nome)
+values(
+	'M', 55, '2018-12-01', '11 5555-2222', 645.22, 1, 
+    'Rua Crodovaldo, 1500, ap525 - SP', 'Zé'
+);
+
+insert into aluno(
+	sexo, idade, data_inscricao_curso, telefone, valor_pago_curso, ativo_sn, endereco, nome)
+values(
+	'M', 55, '2018-12-06', '11 5555-2222', 645.22, 1, 
+    'Avenida Paulista, 1555, ap225 - SP', 'João'
+);
+
+insert into aluno(
+	sexo, idade, data_inscricao_curso, telefone, valor_pago_curso, ativo_sn, endereco, nome)
+values(
+	'F', 30, '2020-01-25', '11 3333-2222', 645.22, 1, 
+    'Rua Francisco Sá, 20, SP', 'Maria'
+);
